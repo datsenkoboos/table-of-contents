10c9102ede79aef43eaede4c5f0e4c5189d4e031
datsenkoboos <adacenkoboos@gmail.com>
2023-09-23 22:34:07 +0700

    TableOfContents done

--- src/components/modules/TableOfContents/TableOfContents.spec.ts
+++ src/components/modules/TableOfContents/TableOfContents.spec.ts
@@ -1,5 +1,5 @@
 import TableOfContents from './TableOfContents.vue';
-import { describe, it, expect, vi, type Mock, afterEach } from 'vitest';
+import { describe, it, expect, vi, type Mock, afterEach, vitest } from 'vitest';
 import { mount, shallowMount } from '@vue/test-utils';
 import { createTestingPinia } from '@pinia/testing';
 import { useAppStore } from '@/stores';
@@ -8,6 +8,8 @@ import { useRoute } from 'vue-router';
 import router from '@/router';
 
 const rootNodeSelector = '[data-testid=rootNode]';
+const toggleIconSelector = '[data-testid=toggleIcon]';
+const toggleButtonSelector = '[data-testid=toggleButton]';
 
 vi.mock('vue-router', async () => {
   const actual =
@@ -76,25 +78,78 @@ describe('TableOfContents', () => {
         );
       }
     });
-    it.skip('appStore.data - should render nested nodes visible when route matches nested nodes links', () => {
-      (useRoute as Mock).mockReturnValueOnce({
-        fullPath: 'nested_node',
+    it('appStore.showTableOfContents - should render nav visible when set to "true"', () => {
+      const wrapper = shallowMount(TableOfContents, {
+        global: {
+          plugins: [createTestingPinia()],
+        },
       });
-      const wrapper = mount(TableOfContents, {
+      expect(wrapper.get('nav').isVisible()).toBe(true);
+    });
+    it('appStore.showTableOfContents - should render nav invisible when set to "false"', () => {
+      const wrapper = shallowMount(TableOfContents, {
         global: {
           plugins: [
             createTestingPinia({
               initialState: {
                 app: {
-                  data: DataMock,
+                  showTableOfContents: false,
                 },
               },
             }),
           ],
         },
       });
+      expect(wrapper.get('nav').isVisible()).toBe(false);
+    });
+    it('appStore.showTableOfContents - should render close toggleIcon when set to "true"', () => {
+      const wrapper = shallowMount(TableOfContents, {
+        global: {
+          plugins: [createTestingPinia()],
+        },
+      });
+      expect(wrapper.get(toggleIconSelector).attributes('icon')).toContain(
+        'close'
+      );
+      expect(wrapper.get(toggleIconSelector).attributes('icon')).not.toContain(
+        'menu'
+      );
+    });
+    it('appStore.showTableOfContents - should render menu toggleIcon when set to "false"', () => {
+      const wrapper = shallowMount(TableOfContents, {
+        global: {
+          plugins: [
+            createTestingPinia({
+              initialState: {
+                app: {
+                  showTableOfContents: false,
+                },
+              },
+            }),
+          ],
+        },
+      });
+      expect(wrapper.get(toggleIconSelector).attributes('icon')).not.toContain(
+        'close'
+      );
+      expect(wrapper.get(toggleIconSelector).attributes('icon')).toContain(
+        'menu'
+      );
+    });
+  });
+  describe('user interactions', () => {
+    it('toggleButton click - should call appStore.toggleTableOfContents', async () => {
+      const wrapper = shallowMount(TableOfContents, {
+        global: {
+          plugins: [createTestingPinia()],
+        },
+      });
+      const appStore = useAppStore();
+      vitest.spyOn(appStore, 'toggleTableOfContents');
 
-      expect(wrapper.get(`[data-testid=nested_node]`).isVisible()).toBe(true);
+      await wrapper.get(toggleButtonSelector).trigger('click');
+
+      expect(appStore.toggleTableOfContents).toHaveBeenCalled();
     });
   });
 });
--- src/components/modules/TableOfContents/TableOfContents.vue
+++ src/components/modules/TableOfContents/TableOfContents.vue
@@ -1,23 +1,54 @@
 <template>
-  <div class="min-w-[300px] border-r-[2px]">
-    <nav
-      class="fixed w-[300px] max-h-[100dvh] pr-5 pl-4 pt-4 pb-8 overflow-y-scroll"
+  <div
+    class="border-r-[2px]"
+    :class="appStore.showTableOfContents ? 'min-w-[300px]' : 'min-w-[40px]'"
+    data-testid="tableOfContents"
+  >
+    <div
+      class="fixed h-[100dvh] overflow-y-scroll"
+      :class="
+        appStore.showTableOfContents
+          ? 'w-[300px] pr-5 pl-4 pb-8 pt-8'
+          : 'w-[40px]'
+      "
     >
-      <ContentNode
-        v-for="rootKey in appStore.data.rootLevelKeys"
-        :key="rootKey"
-        :content-key="rootKey"
-        data-testid="rootNode"
-      />
-    </nav>
+      <button
+        class="text-[22px]"
+        :class="
+          appStore.showTableOfContents
+            ? 'absolute top-4 right-5'
+            : 'pt-4 h-full w-full flex justify-center hover:bg-gray-200'
+        "
+        @click="appStore.toggleTableOfContents()"
+        data-testid="toggleButton"
+      >
+        <Icon
+          :icon="
+            appStore.showTableOfContents
+              ? 'material-symbols:close-rounded'
+              : 'material-symbols:menu-rounded'
+          "
+          data-testid="toggleIcon"
+        />
+      </button>
+      <nav v-show="appStore.showTableOfContents">
+        <ContentNode
+          v-for="rootKey in appStore.data.rootLevelKeys"
+          :key="rootKey"
+          :content-key="rootKey"
+          data-testid="rootNode"
+        />
+      </nav>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import { onBeforeMount } from 'vue';
 import { useAppStore } from '@/stores';
-import { ContentNode } from './components';
+import ContentNode from './components/ContentNode.vue';
 import router from '@/router';
 import { useRoute } from 'vue-router';
+import { Icon } from '@iconify/vue';
 
 const route = useRoute();
 const appStore = useAppStore();
@@ -27,3 +58,22 @@ onBeforeMount(async () => {
   appStore.init(routeKey);
 });
 </script>
+<style lang="scss">
+.slide-enter-active {
+  transition: transform 0.3s;
+}
+
+.slide-leave-active {
+  transition: transform 0.3s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(-100%);
+}
+
+.slide-leave-from,
+.slide-enter-to {
+  transform: translateX(0);
+}
+</style>
