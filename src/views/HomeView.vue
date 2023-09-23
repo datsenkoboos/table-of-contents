542e6bbb6941bbc50cacf7f526bddea0b6efe8c3
datsenkoboos <adacenkoboos@gmail.com>
2023-09-23 17:49:51 +0700

    HomeView done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,4 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="grow h-[2000px]">
   </main>
 </template>
